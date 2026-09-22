176737bed27963ee1aac46c8cc515308537a05c7
Fernando Marchesano Lerma <fernandolerma@macro.com.ar>
2026-09-22 15:31:31 -0300

    Early commit

--- src/main/java/com/techup/gestao_patrimonio_imobiliario/api/auth/CadastroRequest.java
+++ src/main/java/com/techup/gestao_patrimonio_imobiliario/api/auth/CadastroRequest.java
@@ -2,7 +2,7 @@ package com.techup.gestao_patrimonio_imobiliario.api.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Value;
 
@@ -21,7 +21,10 @@ public class CadastroRequest {
     String telefone;
 
     @NotBlank
-    @Size(min = 6)
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$",
+            message = "A senha deve ter ao menos 8 caracteres, uma letra maiuscula, "
+                    + "uma letra minuscula, um numero e um caractere especial")
     String senha;
 
     @NotBlank
